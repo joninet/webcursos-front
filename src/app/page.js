9a3627cfb629ef7b9c901821b59589a7e74b6d5c
jonathan <jon.desplats@gmail.com>
2025-08-06 21:56:22 -0300

    subiendo

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,14 @@
+"use client";
+import { useEffect } from "react";
 export default function Home() {
+  useEffect(()=>{
+    fetch(`http://localhost:3000/courses`)
+    .then((res) => res.json())
+    .then()
+    .catch((err) => {
+      console.log({err})
+    })
+  }, [])
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h2>Web de Cursos</h2>
